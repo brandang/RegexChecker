93be2d773ad51461626aaa4e7b394a399293400a
Brandon <ilikevid3ogames@gmail.com>
2018-05-11 00:09:51 -0400

    Made MainGUI implement methods from interface

--- src/MainGUI.java
+++ src/MainGUI.java
@@ -1,2 +1,12 @@
 public class MainGUI implements RegexFrontend {
+
+    @Override
+    public void updateRegex(String newRegex) {
+
+    }
+
+    @Override
+    public void updateInput(String newInput) {
+
+    }
 }

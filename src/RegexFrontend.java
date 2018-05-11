e30b14a587ea329a1381f64179c0b6ca32cb3d1c
Brandon <brandon.dang@mail.utoronto.ca>
2018-05-11 00:05:47 -0400

    Made changes to RegexFrontend interface.

--- src/RegexFrontend.java
+++ src/RegexFrontend.java
@@ -1,2 +1,17 @@
+/**
+ * Interface that represents the frontend of the program. Allows the backend to tell the frontend what to display.
+ */
 public interface RegexFrontend {
+
+    /**
+     * Update the regex text area.
+     * @param newRegex The new text.
+     */
+    void updateRegex(String newRegex);
+
+    /**
+     * Update the input text area.
+     * @param newInput The new text.
+     */
+    void updateInput(String newInput);
 }

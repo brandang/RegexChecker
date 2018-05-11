d2dc379fd43816a11b7488c7b068cca8d793995a
Brandon <ilikevid3ogames@gmail.com>
2018-05-11 00:02:50 -0400

    Made Launcher open a window upon start.

--- src/Constants.java
+++ src/Constants.java
@@ -1,2 +1,9 @@
 public class Constants {
-} 
+
+    // Window sizes.
+    final public static int DEFAULT_WINDOW_WIDTH = 600;
+    final public static int DEFAULT_WINDOW_HEIGHT = 600;
+
+    // Name of window.
+    final public static String WINDOW_NAME = "RegexChecker";
+}
--- src/Launcher.java
+++ src/Launcher.java
@@ -5,6 +5,7 @@ public class Launcher extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-
+        primaryStage.setTitle(Constants.WINDOW_NAME);
+        primaryStage.show();
     }
 }
